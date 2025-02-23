9232688dcf754703e63ce9db66a954aa7d033dcb
alexfocus <alex92vk@gmail.com>
2025-02-23 22:35:22 +0100

    primer proyecto de un contador de clicks

---
+++ contador/app.js
@@ -0,0 +1,22 @@
+// Selecciona el elemento donde se muestra el contador
+const value = document.getElementById('value');
+// Selecciona todos los botones
+const btns = document.querySelectorAll('.btn');
+
+let count = 0;
+
+// Agrega un listener para cada botón
+btns.forEach(function (btn) {
+  btn.addEventListener('click', function (e) {
+    // Verifica qué botón fue presionado basándose en su clase
+    if (e.currentTarget.classList.contains('decrease')) {
+      count--;
+    } else if (e.currentTarget.classList.contains('reset')) {
+      count = 0;
+    } else if (e.currentTarget.classList.contains('increase')) {
+      count++;
+    }
+    // Actualiza el contenido del span con el nuevo valor
+    value.textContent = count;
+  });
+});
